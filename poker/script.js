5aef066952dd113ba8adc92526ef23e19f03bc65
Ivan Romero <ivanromero5520@gmail.com>
2023-03-15 21:53:06 +0100

    poker

--- poker/script.js
+++ poker/script.js
@@ -1,16 +1,51 @@
 class PokerPlayer {
-  constructor() {
-    this.name
-    this.result
+  constructor(player) {
+    this.name = player + '';
+    this.result = [];
+    }
+  
+   rollDie() {
+    
+    
+     const iPromise = () => {
+        return  new Promise((resolve, reject)  => {
+         setTimeout(() => {
+
+          let array = ["A", "8", "9", "J", "Q", "K"]
+        let rand = Math.floor(Math.random()*array.length);
+        let aValue = array[rand];
+           resolve(aValue)
+          }, 300)
+         } ) 
+        }
+         return  iPromise()
+            
   }
 
-  rollDie() {}
+  async getResult() {
+    
+    for (let index = -1; ++index < 5;) {
+      
+     const result = await this.rollDie();
+      if(this.result.length < 5){
+        this.result.push(result)
+      }
+    }
+   }
+  }
+  const getHandName = () => {
+     if (player === combinations,isFiveOfAKind){
+      console.log('five of a king')
+     }
+   }
+    
+     
 
-  getResult() {}
-}
+     const getScore = () => {
 
-const getHandName = () => {}
+     }
 
-const getScore = () => {}
+     const checkWinner = () => {
 
-const checkWinner = () => {}
+     }
+
---
+++ sumArray/test/alternative.test.js
@@ -0,0 +1,14 @@
+describe('test', () =>{
+       it('should exist a method named "sumArray',() => {
+        expect(typeof sumArray).toEqual('fuction')
+       })
+        it('should it not return null',() => {
+            expect(sumArray,value).not.toBe('null')
+        })
+        it('should it not return undefined', () => {
+            expect(sumArray.value).not.toBe('undefined')
+
+        })
+
+
+})
